a5479719f1701af51024b0dfe206d0f5111952c3
Jose Oshiro <kiros@MacBook-Pro-de-Jose.local>
2020-01-21 03:37:31 +0000

    add xunit test

---
+++ setours.jarvis.testing.xunit/ProviderTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xunit;
+
+namespace setours.jarvis.testing.xunit
+{
+    public class ProviderTest
+    {
+        [Fact]
+        public void Add()
+        {
+            Assert.True(true);
+            Assert.Equal(1, 1);
+            Assert.Equal("Jose", "Jose", ignoreCase:true);
+            Assert.Contains(" josE", "Jose", StringComparison.InvariantCultureIgnoreCase);
+            Assert.StartsWith("Jo", "Jose");
+            Assert.EndsWith("se", "Jose");
+
+        }
+    }
+}
